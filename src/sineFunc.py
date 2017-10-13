58f988b71a6e9f750de9e26ae0c6b7f5a1999752
Sangam Kumar <b150110cs@nitsikkim.ac.in>
2017-10-14 02:41:41 +0530

    Sinc Function add

---
+++ src/sineFunc.py
@@ -0,0 +1,33 @@
+import OpenGL 
+OpenGL.ERROR_ON_COPY = True 
+
+from OpenGL.GLUT import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import math
+
+def display():
+    glClear(GL_COLOR_BUFFER_BIT)
+
+    glColor3f(1.0, 0.0, 0.0)
+
+    x = -10.0
+
+    sinc=0
+
+    glPointSize(2)
+    glBegin(GL_POINTS)
+    while (x<10.0):
+    	sinc = math.sin(x) / x
+    	glVertex2f(x*0.09, sinc*0.09)
+        x = x + 0.1
+    glEnd()
+    glFlush()
+
+    
+glutInit(sys.argv)
+glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB)
+glutInitWindowSize (1500, 1500)
+glutCreateWindow ('Sinc Function')
+glutDisplayFunc(display)
+glutMainLoop()
