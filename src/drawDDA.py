f7ef67a8b6ae82fa73ac748ee41ba3667bb070a2
Sangam Kumar <b150110cs@nitsikkim.ac.in>
2017-10-24 12:01:10 +0530

    Modify src/drawDDA.py

--- src/drawDDA.py
+++ src/drawDDA.py
@@ -2,44 +2,52 @@ from OpenGL.GLUT import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
+def init():
+	glClearColor(0.0,0.0,0.0,0.0)
+	glOrtho(-100,100,-100,100,-1,1)
 
 def ROUND(a):
 	return int(a + 0.5)
 
-def drawDDA():
-	x1,y1,x2,y2 = -7,-5,2,4
-	x,y = x1,y1
-	length = (x2-x1) if (x2-x1) > (y2-y1) else (y2-y1)
-	dx = (x2-x1)/float(length)
-	dy = (y2-y1)/float(length)
-	glClearColor(0, 0, 0, 0)
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-	glPointSize(4)
-	x = (ROUND(x))
-	y =	(ROUND(y))
+def point(x,y):
 
 	glBegin(GL_POINTS)
 	glColor3f(1.0, 0, 0)
-	glVertex2f(x*0.1,y*0.1)
+	glVertex2d(x,y)
 	glEnd()
 
-	for i in range(length):
-		x += dx
-		y += dy
+
+def drawDDA():
+	x1,y1,x2,y2 = -50,-25,20,40
+	x,y = x1,y1
+	dx = (x2-x1)
+	dy = (y2-y1)
+	glClearColor(0, 0, 0, 0)
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+	glPointSize(2)
+
+	if abs(dx)>abs(dy):
+		slope=dx
+
+	else:
+		slope=dy
+
+	xi=dx/slope
+	yi=dy/slope
+	steps=int(slope)
+	for i in range(steps):
+		x = x + xi
+		y = y + yi
 		x = (ROUND(x))
 		y = (ROUND(y))
-		glBegin(GL_POINTS)
-		glColor3f(1.0, 0, 0)
-		glVertex2f(x*0.1,y*0.1)
-		glEnd()
+		point(x,y)
 
 	glFlush()
 
 glutInit(sys.argv)
-
 glutInitDisplayMode(GLUT_RGBA | GLUT_SINGLE | GLUT_DEPTH)
 glutInitWindowSize(500, 500)
 glutCreateWindow('DDALine')
-
+init()
 glutDisplayFunc(drawDDA)
 glutMainLoop()
